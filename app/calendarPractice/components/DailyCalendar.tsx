219fdcceae1667bfa8daf819042d75072b5bae50
jing <jingli1524@gmail.com>
2025-09-29 17:50:58 -0400

    wip

--- app/calendarPractice/components/DailyCalendar.tsx
+++ app/calendarPractice/components/DailyCalendar.tsx
@@ -11,10 +11,19 @@ import {
 import { sample } from '../until/mock-data'
 import { Button } from '@/components/ui/button'
 import { Popover, PopoverContent, PopoverTrigger } from '@/components/ui/popover'
+import { useRef } from 'react'
 
 export const DailyCalendar = () => {
+  const eventContainerRef = useRef<HTMLDivElement>(null)
   const partitioed_events = partition_events(sample)
-  // TODO: render separator lines afte each hour label
+
+  const handleGridClick = (e: React.MouseEvent) => {
+    const element = eventContainerRef.current
+    if (!element) return
+    const rect = element.getBoundingClientRect()
+    const y = e.clientY - rect.top // y position within the element
+    const yWithScroll = y + element.scrollTop
+  }
   return (
     <div className="grid grid-cols-12 p-4 w-full" style={{ height: total_height }}>
       <div
@@ -25,14 +34,14 @@ export const DailyCalendar = () => {
           return (
             <Popover key={i}>
               <PopoverTrigger
-                className="flex items-start w-full col-span-10 hover:cursor-pointer relative"
+                className="flex items-start w-full col-span-10 hover:cursor-pointer relative "
                 style={{ height: hour_height }}
                 onClick={() => {
                   console.log('click', label)
                 }}
               >
                 <div className="pr-2 text-sm opacity-60 whitespace-nowrap">{label}</div>
-                <Separator orientation="horizontal" className="bg-red-300 z-0 w-full absolute" />
+                <Separator orientation="horizontal" className="bg-red-300 w-full absolute" />
               </PopoverTrigger>
               <PopoverContent>
                 <div className="p-2">Add event at {label}</div>
@@ -43,8 +52,10 @@ export const DailyCalendar = () => {
       </div>
 
       <div
-        className="col-start-3 col-span-9 row-start-1 relative z-10"
+        className="col-start-3 col-span-9 row-start-1 relative z-30"
         style={{ height: total_height }}
+        ref={eventContainerRef}
+        onClick={}
       >
         {partitioed_events.map((event, i) => {
           const { top, height } = event_position(event)
